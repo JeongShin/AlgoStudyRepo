6f5e280599aafa78062d8a6d3a5972ab0bc110e7
JeongShin <sjeong1127@gmail.com>
2020-06-20 23:14:43 +0900

    quick sort edited

--- JavaScript/Sort/QuickSort.js
+++ JavaScript/Sort/QuickSort.js
@@ -1,11 +1,15 @@
 /*
-* mid pivot
+* Quick Sort Implementation
 * */
-function partition(arr, left, right) {
+
+// swap function is commonly used
+const swap = (arr, l, r) => {
+    [arr[l], arr[r]] = [arr[r], arr[l]];
+};
+
+/*Mid Pivot Implementation */
+function midPivot(arr, left, right) {
     const pivot = arr[~~((right + left) / 2)];
-    const swap = (arr, l, r) => {
-        [arr[l], arr[r]] = [arr[r], arr[l]];
-    };
     let [l, r] = [left, right];
     while (l <= r) {
         while (arr[l] < pivot)
@@ -21,15 +25,33 @@ function partition(arr, left, right) {
     return l;
 }
 
+/*Left Pivot Implementation */
+function leftPivot(arr, left, right) {
+    const pivot = arr[left];
+    let l = left;
+    const len = arr.length;
+    for (let r = left + 1; r < len; r++) {
+        if (pivot > arr[r]) {
+            l++;
+            swap(arr, l, r);
+        }
+    }
+    swap(arr, left, l);
+    return l;
+}
+
 function quickSort(arr, left = 0, right = arr.length - 1) {
-    let idx = partition(arr, left, right);
-    if (left < idx - 1) // more elements on the left side of pivot
+    if (left < right) {
+        let idx = leftPivot(arr, left, right);
+        // let idx = midPivot(arr, left, right);
         quickSort(arr, left, idx - 1);
-    if (idx < right) // more elements on the right side of pivot
-        quickSort(arr, idx, right);
+        quickSort(arr, idx + 1, right);
+    }
     return arr;
 }
 
 const arr = [3, 1, 5, 8, 2, 2, 9, 4, 1];
-const sorted = quickSort(arr);
-console.log(sorted);
+quickSort(arr);
+console.log(arr);
+
+
